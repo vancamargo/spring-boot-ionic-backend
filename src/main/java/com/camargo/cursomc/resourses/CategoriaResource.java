ebb7db3ee6fdf8f9b4901e8e8c92610d9ab53dc5
Vanessa Camargo <vanessacamargo.v@gmail.com>
2018-10-19 21:00:56 -0300

    dominio

--- src/main/java/com/camargo/cursomc/resourses/CategoriaResource.java
+++ src/main/java/com/camargo/cursomc/resourses/CategoriaResource.java
@@ -1,16 +1,32 @@
 package com.camargo.cursomc.resourses;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.camargo.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "rest ok";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(1, "Escrotorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		
+		
+		
+		return lista;
 	}
 
 }
