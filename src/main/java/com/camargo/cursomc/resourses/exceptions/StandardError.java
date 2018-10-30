8fc07dcae1f395f4a206ae012f6db5e3d84c4e3f
Vanessa Camargo <vanessacamargo.v@gmail.com>
2018-10-30 17:55:33 -0300

    Ajustes nos end point

---
+++ src/main/java/com/camargo/cursomc/resourses/exceptions/StandardError.java
@@ -0,0 +1,32 @@
+package com.camargo.cursomc.resourses.exceptions;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer status;
+	private String msg;
+	private Long timeStamp;
+	
+	public StandardError(Integer status, String msg, Long timeStamp) {
+		super();
+		this.status = status;
+		this.msg = msg;
+		this.timeStamp = timeStamp;
+	}
+	public Integer getStatus() {
+		return status;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public Long getTimeStamp() {
+		return timeStamp;
+	}
+	
+	
+	
+}
--- src/main/java/com/camargo/cursomc/services/CategoriaService.java
+++ src/main/java/com/camargo/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.camargo.cursomc.domain.Categoria;
 import com.camargo.cursomc.repositories.CategoriaRepository;
+import com.camargo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,6 +18,8 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado! id:" + id + ", Tipo" + Categoria.class.getName()));
+		
+		
 		}
 }
