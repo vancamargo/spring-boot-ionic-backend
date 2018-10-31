496944b520784f4ae1212882c00d0639ce5203a8
Vanessa Camargo <vanessacamargo.v@gmail.com>
2018-10-31 20:22:20 -0300

    cidades e estados

---
+++ src/main/java/com/camargo/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.camargo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.camargo.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
