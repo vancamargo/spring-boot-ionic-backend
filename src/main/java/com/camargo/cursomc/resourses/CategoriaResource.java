9649be69223a183a5c71b8998c1665d968793238
Vanessa Camargo <vanessacamargo.v@gmail.com>
2018-11-28 17:47:56 -0200

    put

--- src/main/java/com/camargo/cursomc/resourses/CategoriaResource.java
+++ src/main/java/com/camargo/cursomc/resourses/CategoriaResource.java
@@ -39,6 +39,17 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+
+	@RequestMapping (value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+		
+	
+	
 	
 }
 
