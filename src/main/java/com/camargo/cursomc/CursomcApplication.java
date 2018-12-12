829aefa61955fe2e6fced6a8de2ef4ea9721eb76
Vanessa Camargo <vanessacamargo.v@gmail.com>
2018-12-12 15:51:03 -0200

    Paginacao com parametros na requisicao

--- src/main/java/com/camargo/cursomc/CursomcApplication.java
+++ src/main/java/com/camargo/cursomc/CursomcApplication.java
@@ -69,6 +69,11 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "cama mesa e banho");
+		Categoria cat4 = new Categoria(null, "eletronico");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		
 		Produto p1 = new Produto(null, "computador", 2000.00);
 		Produto p2 = new Produto(null, "computador", 8000.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
@@ -82,7 +87,7 @@ public class CursomcApplication implements CommandLineRunner{
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
